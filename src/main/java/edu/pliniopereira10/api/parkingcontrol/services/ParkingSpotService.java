e803846dac3480fa079d91d1bae9a3b49d8b16d4
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-19 16:26:11 -0300

    if applied, this commit will create the save method in classname ParkingSpotService

--- src/main/java/edu/pliniopereira10/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/edu/pliniopereira10/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,8 +1,11 @@
 package edu.pliniopereira10.api.parkingcontrol.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.pliniopereira10.api.parkingcontrol.models.ParkingSpotModel;
 import edu.pliniopereira10.api.parkingcontrol.repositories.ParkingSpotRepository;
 
 @Service
@@ -10,5 +13,10 @@ public class ParkingSpotService {
 
 	@Autowired
 	ParkingSpotRepository parkingSpotRepository;
+
+	@Transactional
+	public Object save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
 	
 }
