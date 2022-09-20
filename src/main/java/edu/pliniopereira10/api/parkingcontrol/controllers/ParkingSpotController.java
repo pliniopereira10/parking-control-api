d91e035399a4e35b728ef9d0caf35a4101c8994f
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-20 17:45:20 -0300

    If applied, this commit will create the method @DeleteMapping deleteParkingSpot in the classname of the ParkingSpotContoller

--- src/main/java/edu/pliniopereira10/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/edu/pliniopereira10/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +68,18 @@ public class ParkingSpotController {
 
 		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
 	}
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id") UUID id){
+    	Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+    	
+    	if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+		}
+    	
+    	parkingSpotService.delete(parkingSpotModelOptional.get());
+    	
+		return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted succefully");
+    }
 	
 }
